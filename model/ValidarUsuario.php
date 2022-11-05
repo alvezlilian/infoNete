8ff64235cdcdab31cf6718374a9feacf0b7dfdd7
Gustavo27Gonzalez <gustavo27gonzalez27@gmail.com>
2022-11-05 18:11:38 -0300

    hay funciones para validar mail, hashear contraseña, sanitizar datos, falta completar el circuito para invocarlas correctamente

--- model/ValidarUsuario.php
+++ model/ValidarUsuario.php
@@ -15,12 +15,11 @@ class ValidarUsuarioModel
 
     public function validarUsuario($codigo){
 
-
-        $sql = "SELECT email,descripcion from usuario JOIN contrasenia on usuario.id = contrasenia.idUsuario join rol on usuario.idRol=rol.id WHERE usuario.email = '$email' AND contrasenia.clave = '$clave'";
-        $resultado = $this->database->query($sql);
+        $resultado = "SELECT * from contrasenia WHERE contrasenia.codigo = '$codigo' ";
+        //$resultado = $this->database->query($sql);
 
         if(!isset($resultado)||$resultado==NULL){
-            Redirect::doIt("/login/validarLogin");
+            Redirect::doIt("/");
         }
         return $resultado;
     }
