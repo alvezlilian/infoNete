2e78e5c613e2204bfc2e02ad43d5baa67b5fded5
Meistt <aguslive7@gmail.com>
2022-11-22 22:11:56 -0300

    comienzo con el desarrollo de administrador

--- controller/RegistrarseController.php
+++ controller/RegistrarseController.php
@@ -59,6 +59,7 @@ class RegistrarseController
 
     public function validarCodigo(){
         $codigo = $this->model->validarCodigoRegistro($_POST['codigo']);
+        die(var_dump());
         if($codigo){
             $this->renderer->render("loginView.mustache");
         }else{
